4e7ccd711f786c55f6f798a30c03172991915a2c
MatheusWinck <matheusrw05@gmail.com>
2018-08-14 08:31:16 -0300

    criacao metodo subtracao

--- src/calccerta/calculadora.java
+++ src/calccerta/calculadora.java
@@ -1,5 +1,10 @@
 package calccerta;
 
 public class calculadora {
-
+	public int som(int num1, int num2) {
+		return num1 + num2;
+	}
+	public int sub(int num1, int num2){
+		return num1 - num2;
+	}
 }
